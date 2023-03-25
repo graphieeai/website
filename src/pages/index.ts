0ff13a7518f193932df8f62fda9333edf4b52c08
Nikhita <bnikhita2003@gmail.com>
2023-03-25 12:30:23 +0530

    exporting pages

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+// 
+export { default as Home } from './home/home.pages';
+export { default as Dashboard } from './dashboard/dashboard.pages';
+export { default as Solution } from './solutions/solution.pages';
