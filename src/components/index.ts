4084fff2d41a7a4cd0bb64332a5fbabe9637e007
Nikhita <bnikhita2003@gmail.com>
2023-03-25 12:29:41 +0530

    exporting components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from './header/header.components';
+export { default as Hero } from './hero/hero.components';
+export { default as BarChart } from './graphs/barchart';
+export { default as AreaChart } from './graphs/areachart';
